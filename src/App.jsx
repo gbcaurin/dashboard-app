4fdeb54a7338c5358343c54de3eaa6df1075c712
Gabriel Caurin <gabrielcaurin@gmail.com>
2025-02-12 13:23:05 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Login from "./pages/Login/login";
+import Dashboard from "./pages/Dashboard/dashboard";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
+}
+
+export default App;
