4e656dd59fa6b769e7171a6936e177a361af80c3
Gabriel Caurin <gabrielcaurin@gmail.com>
2025-02-14 12:44:00 -0300

    feat: creating signUp page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Login from "./pages/Login/login";
 import Dashboard from "./pages/Dashboard/dashboard";
+import SignUp from "./pages/Sign Up/signup";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/signUp" element={<SignUp />} />
     </Routes>
   );
 }
---
+++ src/pages/Sign Up/signup.jsx
@@ -0,0 +1,38 @@
+import styles from "./signup.module.css";
+
+function SignUp() {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.formContainer}>
+          <form className={styles.form}>
+            <div className={styles.formHeader}>
+              <img src="/images/Rectangular_Logo.png" alt="logo" />
+            </div>
+            <div className={styles.inputGroup}>
+              <input type="text" id="name" autoComplete="off" />
+              <label htmlFor="name">Nome</label>
+            </div>
+            <div className={styles.inputGroup}>
+              <input type="email" id="email" autoComplete="off" />
+              <label htmlFor="email">E-mail</label>
+            </div>
+            <div className={styles.inputGroup}>
+              <input type="password" id="password" autoComplete="off" />
+              <label htmlFor="password">Senha</label>
+            </div>
+            <div className={styles.inputGroup}>
+              <input type="password" id="confirmPassword" autoComplete="off" />
+              <label htmlFor="confirmPassword">Confirmar Senha</label>
+            </div>
+            <button type="submit" className={styles.submitButton}>
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SignUp;
