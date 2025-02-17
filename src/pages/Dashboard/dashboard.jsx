16fea78a5a1cebf3acf77118bb0da45f4a2bb257
Gabriel Caurin <gabrielcaurin@gmail.com>
2025-02-17 13:00:36 -0300

    feat: unlogged message

--- src/pages/Dashboard/dashboard.jsx
+++ src/pages/Dashboard/dashboard.jsx
@@ -1,25 +1,40 @@
 import { useEffect, useState } from "react";
 import { auth } from "../../firebaseConfig";
 import { useNavigate } from "react-router-dom";
+import styles from "./dashboard.module.css";
 
 function Dashboard() {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((currentUser) => {
-      if (currentUser) {
-        setUser(currentUser);
-      } else {
-        setUser(null);
-      }
+      setUser(currentUser);
+      setLoading(false);
     });
 
     return () => unsubscribe();
   }, []);
 
+  if (loading) {
+    return <h2>Carregando...</h2>;
+  }
+
   if (!user) {
-    return <p>Carregando...</p>;
+    return (
+      <>
+        <div className={styles.containerUnlogged}>
+          <h2 className={styles.titleUnlogged}>Você não está logado!</h2>
+          <button
+            onClick={() => navigate("/")}
+            className={styles.buttonUnlogged}
+          >
+            Ir para a página de login
+          </button>
+        </div>
+      </>
+    );
   }
 
   return (
