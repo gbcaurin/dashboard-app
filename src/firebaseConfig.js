5f471e1e3fbd4801ae86d6a5c884c0d314b10bcc
Gabriel Caurin <gabrielcaurin@gmail.com>
2025-02-18 15:43:40 -0300

    feat: looping

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,13 +6,13 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.REACT_API_KEY,
+  authDomain: import.meta.env.REACT_AUTH_DOMAIN,
+  projectId: import.meta.env.REACT_PROJECT_ID,
+  storageBucket: import.meta.env.REACT_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.REACT_MESSAGING_SENDER_ID,
+  appId: import.meta.env.REACT_APP_ID,
+  measurementId: import.meta.env.REACT_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
